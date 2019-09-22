463bb73e108e3e4baca193f53b20eee20871af9a
Appointy <Appointy@DESKTOP-J5VKSQ9>
2019-09-22 17:17:55 +0530

    cxcg

--- Protractor test/JsFiles/configuration.js
+++ Protractor test/JsFiles/configuration.js
@@ -6,7 +6,7 @@ exports.config = {
   framework: 'jasmine',
 //  seleniumAddress: 'http://localhost:4444/wd/hub',
   directConnect: true,
-  specs: [ 'recurringbooking.js'],
+  specs: [ 'payBooking.js.js'],
   
   
   params:{
